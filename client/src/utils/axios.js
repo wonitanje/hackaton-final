6bc92192cbd4e59e7d393797f58707a405a5116d
wonitanje <prod.juswee@gmail.com>
2021-12-04 01:27:49 +1000

    heroku

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 axios.defaults.withCredentials = true
-axios.defaults.baseURL = 'http://localhost:8000/'
+// axios.defaults.baseURL = 'http://localhost:8000/'
+axios.defaults.baseURL = 'https://rustt.a1.srv.anodev.ru/'
 
 export default axios
